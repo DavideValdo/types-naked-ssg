af8f6e5f0f01e785dc08bbbd714caf6252e02275
Davide Biagini <mdev@Davides-Air.homenet.telecomitalia.it>
2023-05-19 15:28:33 +0200

    deploy

--- types/ProxyConfiguration.ts
+++ types/ProxyConfiguration.ts
@@ -1,7 +1,8 @@
 import { PageConfiguration } from "./PageConfiguration"
+import ProxyEntry from "./ProxyEntry"
 
 type ProxyConfiguration = {
-  data: Array<unknown>
+  data: Array<ProxyEntry>
   templateName: string
   localConfigFactory: (data: unknown) => Partial<PageConfiguration>
 }

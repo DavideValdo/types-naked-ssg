22916635c606d3d23b1448a4ad788b587f4a6719
Davide Biagini <mdev@Davides-Air.homenet.telecomitalia.it>
2023-05-19 15:47:45 +0200

    deploy

--- types/ProxyEntry.ts
+++ types/ProxyEntry.ts
@@ -1,5 +1,10 @@
-type ProxyEntry = {
+type Payload = {
   slug: string
 } & Record<string, unknown>
+
+/**
+ * The key is the culture code.
+ */
+type ProxyEntry = Record<string, Payload>
 
 export default ProxyEntry

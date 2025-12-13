1e82be5aaa113b59ccff288674fea6e25446ef0f
Davide Biagini (dev) <davide.valdo.biagini@gmail.com>
2025-12-13 15:54:46 +0100

    deploy

--- types/ProxyConfiguration.ts
+++ types/ProxyConfiguration.ts
@@ -1,6 +1,8 @@
+import { PageConfiguration } from "./PageConfiguration"
 import type { ProxyCultureRecordsMap } from "./ProxyCultureRecordsMap"
 
 export type ProxyConfiguration<PT> = {
   fetchData: () => Promise<ProxyCultureRecordsMap<PT>>
+  fetchConfig: () => Promise<PageConfiguration>
   layoutName: string
 }
